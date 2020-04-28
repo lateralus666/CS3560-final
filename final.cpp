0502efde724cc7f74d4314afc6c2553eae86065f
lateralus666 <42786822+lateralus666@users.noreply.github.com>
2020-04-28 11:46:39 -0400

    Update final.cpp
    added functions

--- final.cpp
+++ final.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
+int count_line(string choice);
+int count_char(string choice);
 int main(){
 
+}
+int count_line(string choice){
+    int line_count = 0;
+    if ( choice.back() == '\n'){
+        line_count++;
+    }
+    return line_count;
+}
+int count_char(string choice){
+    int char_count = choice.size();
+    return char_count;
 }
