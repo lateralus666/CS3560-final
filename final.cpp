d2e79e6c43e459cb313cdfb3c64374ea0fb14b4c
lateralus666 <42786822+lateralus666@users.noreply.github.com>
2020-04-28 11:53:22 -0400

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -4,7 +4,15 @@ using namespace std;
 int count_line(string choice);
 int count_char(string choice);
 int main(){
-
+string choice;
+int line_count = 0;
+int char_count = 0;
+cout << "Enter a Word" << endl;
+cin >> choice;
+line_count = count_line (choice);
+char_count = count_char(choice);
+cout << "line count = " << line_count << endl;
+cout << "char count = " << char_count << endl;
 }
 int count_line(string choice){
     int line_count = 0;
